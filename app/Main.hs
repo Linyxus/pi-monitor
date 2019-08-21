df4f4a8904656405bcaa4c933f547dd8a5e87b5b
Linyxus <linyxus00@gmail.com>
2019-08-21 16:35:40 +0800

    Enable CORS

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,7 @@ module Main where
 import Web.Scotty
 import Network.Wai.Middleware.RequestLogger
 import Network.Wai.Middleware.HttpAuth
+import Network.Wai.Middleware.Cors
 import Control.Monad.IO.Class (liftIO)
 
 import Service.SMS.Route as SMSR
@@ -15,6 +16,7 @@ main :: IO ()
 main = do
   smsCtxt <- SMSC.initialize
   scotty 3000 $ do
+    middleware simpleCors
     middleware logStdoutDev
     middleware $ basicAuth (\u p -> return True) "My Realm"
     SMSR.routes smsCtxt
