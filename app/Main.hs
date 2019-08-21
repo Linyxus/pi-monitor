b4369913c1dec039c98f30493aa0bd4e0048eba0
Linyxus <linyxus00@gmail.com>
2019-08-21 23:48:48 +0800

    Provide cors headers for debugging.

--- app/Main.hs
+++ app/Main.hs
@@ -12,11 +12,27 @@ import Control.Monad.IO.Class (liftIO)
 import Service.SMS.Route as SMSR
 import Service.SMS.Context as SMSC
 
+import Network.Wai (requestMethod, Request)
+import Network.HTTP.Types.Method
+
+isNotOptionReq :: Request -> IO Bool
+isNotOptionReq req = return $ requestMethod req /= methodOptions
+
+-- corsPolicy :: CorsResourcePolicy
+-- corsPolicy = CorsResourcePolicy { corsOrigins = Nothing
+--                                 , corsMethods = [ methodGet, methodPost
+--                                                 , methodHead, methodOption ]}
+
 main :: IO ()
 main = do
   smsCtxt <- SMSC.initialize
   scotty 3000 $ do
-    middleware simpleCors
     middleware logStdoutDev
-    middleware $ basicAuth (\u p -> return True) "My Realm"
+    middleware $ basicAuth (\_ _ -> return True) "My Realm" { authIsProtected = isNotOptionReq }
+    -- middleware $ cors (const . pure $ corsPolicy )
+    options (regex "^(.*)$") $ do
+      liftIO $ putStrLn "Options! Catch you!"
+      addHeader "Access-Control-Allow-Origin" "*"
+      addHeader "Access-Control-Allow-Methods" "POST, GET, OPTIONS"
+      addHeader "Access-Control-Allow-Headers" "*"
     SMSR.routes smsCtxt
--- src/Service/SMS/Route.hs
+++ src/Service/SMS/Route.hs
@@ -5,15 +5,19 @@ module Service.SMS.Route where
 import Web.Scotty
 import Service.SMS.Context (SMSContext (..))
 import Service.SMS.Handler
+import Control.Monad.IO.Class (liftIO)
+
+corsHeader = do
+  addHeader "Access-Control-Allow-Origin" "*"
 
 routes :: SMSContext -> ScottyM ()
 routes ctxt = do
   let aliveDb = aliveTable ctxt
       msgDb = msgTable ctxt
   get "/api/sms/hello" $ text "Hello"
-  post "/api/sms/alive" $ postAliveHandler aliveDb
-  get "/api/sms/alive" $ getAliveHandler aliveDb
+  post "/api/sms/alive" $ postAliveHandler aliveDb >> corsHeader
+  get "/api/sms/alive" $ getAliveHandler aliveDb >> corsHeader
   post "/api/sms/message" $ do
     msg <- param "msg"
-    postMessageHandler msgDb msg
-  get "/api/sms/message" $ getMessageHandler msgDb
+    postMessageHandler msgDb msg >> corsHeader
+  get "/api/sms/message" $ getMessageHandler msgDb >> corsHeader
